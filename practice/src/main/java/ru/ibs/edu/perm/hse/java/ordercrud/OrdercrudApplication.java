b9b90714b733bb360049b5b08457d8e65d52da57
admin <khvalentin@ibs.ru>
2019-02-04 11:06:06 +0300

    Initial commit

---
+++ practice/src/main/java/ru/ibs/edu/perm/hse/java/ordercrud/OrdercrudApplication.java
@@ -0,0 +1,14 @@
+package ru.ibs.edu.perm.hse.java.ordercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdercrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdercrudApplication.class, args);
+	}
+
+}
+
---
+++ practice/src/test/java/ru/ibs/edu/perm/hse/java/ordercrud/OrdercrudApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.ibs.edu.perm.hse.java.ordercrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrdercrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
