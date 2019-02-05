d6fcb0c2d975c3df8f924bc527c91f450d827c74
Максим Гильман <mmgilman@edu.hse.ru>
2019-02-05 12:38:28 +0500

    PreparedTOIBS
    Add to IBS Git

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.*;
-
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.example.demo.*;
 
 
 @SpringBootApplication
@@ -12,27 +15,39 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-        // not works
-        //Test(args);
+         Test(args);
 
 	}
 
-    //TODO:Поставить драйвер postgres на сервер?
-    public static final String SELECT_QUERY = "SELECT * FROM PEOPLE";
+    //TODO:Поставить драйвер postgres?
+	public static final Integer countOfRows = 10;
+	//takes limited count of rows in database, or all if counter = 0;
+    public static final String SELECT_QUERY = countOfRows==0? "SELECT * FROM PEOPLE":"SELECT * FROM PEOPLE LIMIT "+countOfRows;
 	public static void Test(String[] args) {
 		String host = "jdbc:postgresql://35.190.193.191:5432/postgres";
 		String uName = "postgres";
 		String uPass = "Passw0rd!";
+        try {
 
+            Class.forName("org.postgresql.Driver");
+
+        } catch (ClassNotFoundException e) {
+
+
+            e.printStackTrace();
+            return;
+
+        }
         //there is no suitable driver exception
         try (Connection conn = DriverManager.getConnection(host, uName, uPass);
 			 Statement stmt = conn.createStatement();
 			 ResultSet rs = stmt.executeQuery(SELECT_QUERY)) {
-
-			while (rs.next()) {
- 				int id = rs.getInt("id");
-				String somePropertyValue = rs.getString(id);
-                 System.out.print("\nSmoke success\n");
+			int columns = rs.getMetaData().getColumnCount();
+			while(rs.next()){
+				for (int i = 1; i <= columns; i++){
+					System.out.print(rs.getString(i) + "\t");
+				}
+				System.out.println();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
