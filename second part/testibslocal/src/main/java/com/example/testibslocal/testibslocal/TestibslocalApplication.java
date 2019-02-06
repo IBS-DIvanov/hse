593734e6f2b12f7aa733e4b7314f57149f0e4d1b
f883 <souprise@ya.ru>
2019-02-07 00:17:43 +0500

    second part initiated

---
+++ second part/testibslocal/src/main/java/com/example/testibslocal/testibslocal/TestibslocalApplication.java
@@ -0,0 +1,14 @@
+package com.example.testibslocal.testibslocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestibslocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestibslocalApplication.class, args);
+	}
+
+}
+
