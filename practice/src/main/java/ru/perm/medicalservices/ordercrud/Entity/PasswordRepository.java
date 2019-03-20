3fb2c496f752a5a9acc3fb59d46f5014feda7f19
Reinger <dvbolotov@edu.hse.ru>
2019-03-20 14:19:06 +0500

    Create @Repository

---
+++ practice/src/main/java/ru/perm/medicalservices/ordercrud/Entity/PasswordRepository.java
@@ -0,0 +1,6 @@
+package ru.perm.medicalservices.ordercrud.Entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PasswordRepository extends CrudRepository<PasswordEntity, Long> {
+}
---
+++ practice/src/main/java/ru/perm/medicalservices/ordercrud/Entity/PostRepository.java
@@ -0,0 +1,6 @@
+package ru.perm.medicalservices.ordercrud.Entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<PostEntity, Integer> {
+}
