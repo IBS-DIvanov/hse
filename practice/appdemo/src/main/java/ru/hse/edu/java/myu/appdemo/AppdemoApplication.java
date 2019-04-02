736dbe59265d2f4dac4a398a39d1b7893bc35635
Yulia Varushkina <yuliavarushkina@yandex.ru>
2019-04-03 01:33:34 +0500

    Initiation

---
+++ practice/appdemo/src/main/java/ru/hse/edu/java/myu/appdemo/AppdemoApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.edu.java.myu.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdemoApplication.class, args);
+	}
+
+}
