fae28b15e5d9b4b54afe848dc2e5e19b482e364f
RakovaAnna <44291028+RakovaAnna@users.noreply.github.com>
2019-06-17 01:49:05 +0500

    Task 2

---
+++ practice/src/main/java/com/testproject/reqcrud/ReqcrudApplication.java
@@ -0,0 +1,13 @@
+package com.testproject.reqcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqcrudApplication.class, args);
+	}
+
+}
